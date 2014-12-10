8ff4b77908f82dbb6d0f18e974d4be66fda86c49
Christian Holmstrand <christian.holmstrand@starcounter.com>
2014-12-10 09:14:47 +0100

    Added launcherhook for search for users and usergroups if user is authorized.

---
+++ src/Server/Partials/Administrator/SearchResult.json.cs
@@ -0,0 +1,25 @@
+using Starcounter;
+
+namespace UserAdminApp.Server.Partials.Administrator {
+    [SearchResult_json]
+    partial class SearchResult : Page {
+
+        [SearchResult_json.Users]
+        partial class UserItem : Json {
+            public string UserUri {
+                get {
+                    return "/launcher/workspace/admin/users/" + Data.GetObjectID();
+                }
+            }
+        }
+
+        [SearchResult_json.Groups]
+        partial class GroupItem : Json {
+            public string GroupUri {
+                get {
+                    return "/launcher/workspace/admin/usergroups/" + Data.GetObjectID();
+                }
+            }
+        }
+    }
+}
